6d383d439ca054dbdadcb5380820ba493ceb1307
ToandroMananjara <mahasalotra@gmail.com>
2023-07-17 18:20:22 +0000

    version final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,14 @@
 let menuOpen = document.getElementById('menu-open')
 let menuClose = document.getElementById('menu-close')
+let menuToggle= document.getElementById('menu-toggle')
 
 menuOpen.onclick= function(){
     menuClose.style.display="block"
     menuOpen.style.display="none"
+    menuToggle.style.display="flex"
     menuClose.onclick=function() {
         menuClose.style.display="none"
         menuOpen.style.display="block"
+        menuToggle.style.display="none"
     }
 }
